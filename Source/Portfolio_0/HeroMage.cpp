ca06fa3d583c6f4313083636481646345c6faad5
jiwooJang-02 <gusung10235@gmail.com>
2023-08-06 00:40:14 +0900

    Add Mage Basic Animation
    Add Mage Basic Animation

---
+++ Source/Portfolio_0/HeroMage.cpp
@@ -0,0 +1,93 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HeroMage.h"
+#include <EnhancedInputComponent.h>
+#include "AnimInstanceHeroMage.h"
+
+AHeroMage::AHeroMage()
+	:IsFlyingState(false), IsSpellState(false)
+{
+}
+
+void AHeroMage::OnFlying()
+{
+	IsFlyingState = true;
+}
+
+void AHeroMage::OnLanding()
+{
+	IsFlyingState = false;
+}
+
+void AHeroMage::OnNormalAttackSpell()
+{
+	//Add Need Attack Code
+
+	IsSpellState = true;
+}
+
+void AHeroMage::OnSpellEnd()
+{
+	IsSpellState = false;
+}
+
+void AHeroMage::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AHeroMage::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AHeroMage::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		// Flying
+		EnhancedInputComponent->BindAction(FlyingAction, ETriggerEvent::Triggered, this, &AHeroMage::Flying);
+
+		// NormalAttackSpell
+		EnhancedInputComponent->BindAction(NormalAttackSpellAction, ETriggerEvent::Triggered, this, &AHeroMage::NormalAttackSpell);
+	}
+}
+
+void AHeroMage::Flying(const FInputActionValue& Value)
+{
+	UAnimInstanceHeroMage* AnimInstanceHeroMage = Cast<UAnimInstanceHeroMage>(GetMesh()->GetAnimInstance());
+	if (!AnimInstanceHeroMage)
+	{
+		return;
+	}
+
+	if (AnimInstanceHeroMage->IsFlying)
+	{
+		PlayAnimMontage(Landing_Montage);
+	}
+	else
+	{
+		PlayAnimMontage(Flying_Montage);
+	}
+}
+
+void AHeroMage::NormalAttackSpell(const FInputActionValue& Value)
+{
+	UAnimInstanceHeroMage* AnimInstanceHeroMage = Cast<UAnimInstanceHeroMage>(GetMesh()->GetAnimInstance());
+	if (!AnimInstanceHeroMage)
+	{
+		return;
+	}
+
+	if (AnimInstanceHeroMage->IsFlying)
+	{
+		PlayAnimMontage(NormalAttackSpell_Fly_Montage);
+	}
+	else
+	{
+		PlayAnimMontage(NormalAttackSpell_Montage);
+	}
+}
