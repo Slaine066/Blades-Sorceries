2adfff60e8136336333ad8b0c10029856d290352
constminjun <gjg3360@naver.com>
2023-08-24 13:56:16 +0900

    Add Golem
    Add Golem

---
+++ Source/Portfolio_0/MobGolem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CharacterMob.h"
+#include "MobGolem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PORTFOLIO_0_API AMobGolem : public ACharacterMob
+{
+	GENERATED_BODY()
+	
+};
